77f3090b3b32bd8f9846c0d66663c26bab768cf1
D3kion <dekion.dev@gmail.com>
2021-08-09 19:48:04 +0300

    Implemented BitArray

--- src/BitArray.js
+++ src/BitArray.js
@@ -1,9 +1,38 @@
 class BitArray {
-  // Bit-length of the underlying array
-  getLength() {}
+  _bits = [];
 
-  // Returns bit value by index
-  getAt(index) {}
+  /**
+   * @param {string} bits
+   */
+  constructor(bits) {
+    if (typeof bits !== 'string') {
+      console.error('Bits must be "string" type!');
+      return;
+    }
+
+    this._bits = bits.split('').map((x) => +x);
+  }
+
+  /**
+   * Bit-length of the underlying array
+   *
+   * @returns {number}
+   */
+  getLength() {
+    return this._bits.length;
+  }
+
+  /**
+   * Returns bit value by index
+   *
+   * @param {number} index
+   * @returns {number|null}
+   */
+  getAt(index) {
+    if (index >= this.getLength()) return null;
+
+    return this._bits[index];
+  }
 }
 
 module.exports = BitArray;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-const BitArray = require('./BitArray');
 const Encoder = require('./Encoder');
 const Decoder = require('./Decoder');
 
 const input = [3, -15, 123, -31, 0, 42];
-const expectedGamma = new BitArray(
-  00111000001001000000000010001010100000000100110100000001111110000001010101
-);
-const expectedDelta = new BitArray(
-  011110011000100000100100010101000100100110100001101111100111010101
-);
+const expectedGamma =
+  '00111000001001000000000010001010100000000100110100000001111110000001010101';
+const expectedDelta =
+  '011110011000100000100100010101000100100110100001101111100111010101';
 
 const encoder = new Encoder();
 const actualGamma = encoder.Gamma(input);
